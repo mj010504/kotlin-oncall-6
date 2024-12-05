9164a507c7594172436c9c7e816ced5c365c08ac
mj010504 <mj010504@naver.com>
2024-12-05 22:15:35 +0900

    test: 순번 입력 관련 테스트 추가

---
+++ src/test/kotlin/oncall/OnCallOrderTest.kt
@@ -0,0 +1,87 @@
+package oncall
+
+import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
+import camp.nextstep.edu.missionutils.test.NsTest
+import oncall.domain.OnCallOrder
+import oncall.domain.OnCallSchedule
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ValueSource
+
+class OnCallOrderTest : NsTest() {
+    @Test
+    fun `평일 순번에 중복된 인원이 있으면 예외가 발생한다`() {
+        val weekdayOrder = mutableListOf("a", "a", "b", "c")
+        val holidayOrder = mutableListOf("a", "b", "c")
+
+        assertThrows<IllegalArgumentException> {
+            OnCallOrder(weekdayOrder, holidayOrder)
+        }
+    }
+
+    @Test
+    fun `휴일 순번에 중복된 인원이 있으면 예외가 발생한다`() {
+        val weekdayOrder = mutableListOf("a", "b", "c")
+        val holidayOrder = mutableListOf("a", "a", "b", "c")
+
+        assertThrows<IllegalArgumentException> {
+            OnCallOrder(weekdayOrder, holidayOrder)
+        }
+    }
+
+    @Test
+    fun `평일 순번과 휴일 순번 인원들의 이름들이 일치하지 않으면 예외가 발생한다`() {
+        val weekdayOrder = mutableListOf("a", "b", "c")
+        val holidayOrder = mutableListOf("a", "b", "e")
+
+        assertThrows<IllegalArgumentException> {
+            OnCallOrder(weekdayOrder, holidayOrder)
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideInvalidOrder")
+    fun `평일 순번과 휴일 순번을 올바르게 입력하지 않을시 다시 입력받는다`(pair: Pair<String, String>) {
+        assertSimpleTest {
+            run(
+              "4, 토",
+                pair.first,
+                pair.second,
+                "4,토",
+                "a,b,c",
+                "c,b,a"
+            )
+            assertThat(output()).contains(
+                ERROR,
+                RETRY_INPUT,
+                """
+                4월 1일 토 c
+                4월 2일 일 b
+                4월 3일 월 a
+                4월 4일 화 b
+                4월 5일 수 c
+                """.trimIndent()
+            )
+        }
+    }
+
+    override fun runMain() {
+        main()
+    }
+
+    companion object {
+        private const val ERROR = "[ERROR]"
+        private const val RETRY_INPUT = "다시 입력해 주세요."
+
+        @JvmStatic
+        fun provideInvalidOrder(): List<Pair<String, String>> {
+            return listOf(
+               Pair("a,b,c", "a,b,e"),
+               Pair("a,b,a", "a,b,c")
+            )
+        }
+    }
+}
--- src/test/kotlin/oncall/OnCallSchedultTest.kt
+++ src/test/kotlin/oncall/OnCallSchedultTest.kt
@@ -1,12 +1,13 @@
 package oncall
 
+import camp.nextstep.edu.missionutils.test.NsTest
 import oncall.domain.OnCallSchedule
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
-class OnCallSchedultTest {
+class OnCallSchedultTest : NsTest() {
     @ParameterizedTest
     @ValueSource(ints = [-1, 0, 13, 99])
    fun `비상 근무를 배정할 월은 1 ~ 12 사이의 정수이다`(month : Int) {
@@ -21,5 +22,9 @@ class OnCallSchedultTest {
         assertThrows<IllegalArgumentException> {
             OnCallSchedule(1, startDay)
         }
+    }
+
+    override fun runMain() {
+        main()
     }
 }
